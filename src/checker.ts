c9fd463270bc248e91e145ec1551f4dbd847ac11
Khánh Hoàng <hoangchaukhanh93@gmail.com>
2021-03-15 08:38:19 +0700

    fix(typo): correct grammar of missing package.json error message

--- src/checker.ts
+++ src/checker.ts
@@ -3,8 +3,6 @@ import { getFileStats, withCurrentDir } from "./process";
 export async function checkPackageJson() {
   const stats = await getFileStats(withCurrentDir("package.json"));
   if (!stats) {
-    throw new Error(
-      "package.json is not found. Must be run in existing project directory is required.",
-    );
+    throw new Error("package.json is not found. Must be run in existing project directory.");
   }
 }
